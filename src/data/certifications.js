342c5e5850de77f62309c434176cb6cefb46b21f
محمد ثروت <mhmdthrwt744@gmail.com>
2026-09-05 15:25:50 +0300

    Add Data Entry Training certificate from Maharat

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -4,6 +4,8 @@ export const certificationsData = [
     title: 'Advanced React Development',
     issuer: 'Meta',
     date: '2023',
+    code: 'CERT_001',
+    category: 'Frontend',
     link: '#',
   },
   {
@@ -11,6 +13,8 @@ export const certificationsData = [
     title: 'Node.js API Masterclass',
     issuer: 'Udemy',
     date: '2023',
+    code: 'CERT_002',
+    category: 'Backend',
     link: '#',
   },
   {
@@ -18,6 +22,17 @@ export const certificationsData = [
     title: 'MongoDB Developer Certification',
     issuer: 'MongoDB University',
     date: '2022',
+    code: 'CERT_003',
+    category: 'Database',
+    link: '#',
+  },
+  {
+    id: 4,
+    title: 'Data Entry Training',
+    issuer: 'Maharat - Ministry of Communications & Information Technology',
+    date: '2025',
+    code: 'CERT_004',
+    category: 'Data Entry',
     link: '#',
   }
 ];
